3f3f58ff703fb9021a8276c21dfe858c289b3889
kannan <kannans@datasirpi.com>
2022-12-08 17:00:08 +0530

    list render

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,41 @@
+<!--when we build web application the common scenario display the list of item (ex.list of name) repeat the html for each item in the list .
+in vue js we archive using v-for directive, (array of string, array of object, array of aaray)-->
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h2 v-for="(name,index) in names" :key="name">{{index}} {{name}}</h2>
+    <h2 v-for="name in fullNames" :key="name.first">{{name.first}} {{name.last}}</h2>
+    
+    <!-- <h2 v-for="actor in actors" :key="actor.name"></h2>
+  <h2>{{actor.name}}</h2>
+  <h3 v-for="movie in actor.movies" :key="movie">{{movie}}</h3> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data(){
+    return {
+      names: ['Tom', 'harry', 'peter'],
+      fullNames: [
+        {first:'tom',last:'wayne'},
+        {first:'harry',last:'kent'},
+        {first:'peter',last:'parker'},
+    ],
+    // actors: [
+    //   {
+    //     name:'Christian Bale',
+    //     movies: ['Batman','The Prestiage'],
+    //   },
+    //   {
+    //     name:'Di Caprio',
+    //     movies: ['Titan','The Inception'],
+    //   },
+    // ],
+    }
+  },
 }
 </script>
 
